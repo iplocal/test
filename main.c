1d4e17f5729440b41f22d6c215964f7070db5220
Xiaodong Xu <stid.smth@gmail.com>
2010-11-22 22:18:46 +0800

    Modify main.c, add test.h

--- main.c
+++ main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+void prnName(struct stName *mySt)
+{
+    printf("Number = %d, ", mySt->uiNum);
+    printf("Letter = %c.\n", mySt->ucLtr);
+
+    return;
+}
+
 int main(int argc, char *argv[])
 {
     printf("Hello, world!\n");
---
+++ test.h
@@ -0,0 +1,14 @@
+#ifndef _TEST_H
+#define _TEST_H
+
+/* Data types */
+struct stName {
+    unsigned int uiNum;
+    unsigned char ucLtr;
+};
+
+/* Function declarations */
+void showName(struct stName *mySt);
+
+#endif /* _TEST_H */
+
